f578e3bf1d918552334719cf9d42922cf658509f
Mmmariuz <mariushauge.93@gmail.com>
2025-02-24 13:28:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
-
-
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-});
-
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-});
-
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-});
-
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
+const toggleButton = document.getElementById('toggle-button');
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
 });
 
